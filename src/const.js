6ed8a388b2888bd987c14a5e2e66ac935f3b929f
vitalymuravyev <vitalymuravyev@gmail.com>
2020-08-17 21:40:13 +0300

    correct comments

--- src/const.js
+++ src/const.js
@@ -1,22 +1,82 @@
 export const SENTENCES = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`. `);
 
-export const TITLES = [`The Dance of Life`, `Sagebrush Trail`, `The Man with the Golden Arm`, `Santa Claus Conquers the Martians`,
-  `Popeye the Sailor Meets Sindbad the Sailor`, `The Great Flamarion`, `Made for Each Other`];
+export const TITLES = [
+  `The Dance of Life`,
+  `Sagebrush Trail`,
+  `The Man with the Golden Arm`,
+  `Santa Claus Conquers the Martians`,
+  `Popeye the Sailor Meets Sindbad the Sailor`,
+  `The Great Flamarion`,
+  `Made for Each Other`
+];
 
-export const ACTIONS = [`Drama`, `Action`, `Mystery`, `Horor`, `Musical`, `Adventure`, `Comedy`, `Fantasy`, `Science fiction`];
+export const ACTIONS = [
+  `Drama`,
+  `Action`,
+  `Mystery`,
+  `Horor`,
+  `Musical`,
+  `Adventure`,
+  `Comedy`,
+  `Fantasy`,
+  `Science fiction`
+];
 
-export const POSTERS = [`made-for-each-other.png`, `popeye-meets-sinbad.png`, `sagebrush-trail.jpg`, `santa-claus-conquers-the-martians.jpg`, `the-dance-of-life.jpg`, `the-great-flamarion.jpg`, `the-man-with-the-golden-arm.jpg`];
+export const POSTERS = [
+  `made-for-each-other.png`,
+  `popeye-meets-sinbad.png`,
+  `sagebrush-trail.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `the-dance-of-life.jpg`,
+  `the-great-flamarion.jpg`,
+  `the-man-with-the-golden-arm.jpg`
+];
 
 export const EMOJIES = [`angry.png`, `puke.png`, `sleeping.png`, `smile.png`];
 
 export const AUTHORS = [`Tim Macoveev`, `John Doe`, `Kipchoge`, `Jolly Woo`, `Mr Smith`];
 
-export const DIRECTORS = [`Martin Scorsese`, ` Alfred Hitchcock`, `Steven Spielberg`, `Roman Polanski`, `Ridley Scott`, `Danny Boyle`, `Sofia Coppola`, `Stanley Kubrick`];
+export const DIRECTORS = [
+  `Martin Scorsese`,
+  ` Alfred Hitchcock`,
+  `Steven Spielberg`,
+  `Roman Polanski`,
+  `Ridley Scott`,
+  `Danny Boyle`,
+  `Sofia Coppola`,
+  `Stanley Kubrick`
+];
 
-export const WRITERS = [`Preston Sturges`, `Woody Allen`, `Akira Kurosawa`, `Charles Chaplin`, `Billy Wilder`, `Paul Schrader`,
-  `Neil Simon`, `Francis Ford Coppola`, `Federico Fellini`, `Ingmar Bergman`, `Mel Brooks`];
+export const WRITERS = [
+  `Preston Sturges`,
+  `Woody Allen`,
+  `Akira Kurosawa`,
+  `Charles Chaplin`,
+  `Billy Wilder`,
+  `Paul Schrader`,
+  `Neil Simon`,
+  `Francis Ford Coppola`,
+  `Federico Fellini`,
+  `Ingmar Bergman`,
+  `Mel Brooks`
+];
 
-export const ACTORS = [`Liam Neeson`, `Kurt Russell`, `Christopher Reeve`, `Drew Barrymore`, `Marilyn Monroe`, `Sally Field`, `Tim Allen`,
-  `Bruce Lee`, `Shirley Temple`, `Reese Witherspoon`, `Andy Griffith`, `Julie Andrews`, `Jennifer Aniston`, `Al Pacino`, `Hugh Jackman`];
+export const ACTORS = [
+  `Liam Neeson`,
+  `Kurt Russell`,
+  `Christopher Reeve`,
+  `Drew Barrymore`,
+  `Marilyn Monroe`,
+  `Sally Field`,
+  `Tim Allen`,
+  `Bruce Lee`,
+  `Shirley Temple`,
+  `Reese Witherspoon`,
+  `Andy Griffith`,
+  `Julie Andrews`,
+  `Jennifer Aniston`,
+  `Al Pacino`,
+  `Hugh Jackman`
+];
 
 export const COUNTRIES = [`USA`, `Italy`, `France`, `Australia`, `Great Britan`, `Germany`];
--- src/mock/film.js
+++ src/mock/film.js
@@ -1,40 +1,52 @@
 import {getRandom, getRandomInteger, generateRandomItem, getRandomBoolean} from "../utils";
 import {SENTENCES, TITLES, ACTIONS, POSTERS, EMOJIES, AUTHORS, DIRECTORS, WRITERS, ACTORS, COUNTRIES} from "../const";
 
+const MINUTES_IN_HOUR = 60;
+const MAX_SENTENCE = 5;
+const MAX_COMMENTS_COUNT = 5;
+const MAX_DATE_GAP = 25550;
+
+const Duration = {
+  MIN: 50,
+  MAX: 100,
+};
+const WritersCount = {
+  MIN: 1,
+  MAX: 2,
+};
+const ActorsCount = {
+  MIN: 3,
+  MAX: 5,
+};
+const GenresCount = {
+  MIN: 1,
+  MAX: 3,
+};
 
 const getRandomDuration = () => {
-  const minutesInHour = 60;
-  const Duration = {
-    MIN: 50,
-    MAX: 100,
-  };
-
   const duration = getRandomInteger(Duration.MIN, Duration.MAX);
 
-  return duration >= minutesInHour ? `${Math.trunc(duration / minutesInHour)}h ${duration % minutesInHour}m` : `${duration}m`;
+  return duration >= MINUTES_IN_HOUR ? `${Math.trunc(duration / MINUTES_IN_HOUR)}h ${duration % MINUTES_IN_HOUR}m` : `${duration}m`;
 };
 
 const getRandomDescription = () => {
-  const MAX_SENTENCE = 5;
   const sentenceCount = getRandomInteger(1, MAX_SENTENCE);
   let description = ``;
   for (let i = 0; i < sentenceCount; i++) {
-    const randomSentence = SENTENCES[getRandomInteger(SENTENCES.length - 1)];
+    const randomSentence = generateRandomItem(SENTENCES);
     description += ` ${randomSentence}.`.trim();
   }
   return description;
 };
 
 const getComment = () => ({
-  post: SENTENCES[getRandomInteger(SENTENCES.length - 1)],
+  post: generateRandomItem(SENTENCES),
   author: generateRandomItem(AUTHORS),
   date: getRandomDate(),
   emoji: generateRandomItem(EMOJIES),
 });
 
 const getComments = () => {
-  const MAX_COMMENTS_COUNT = 5;
-
   const commentsCount = getRandomInteger(MAX_COMMENTS_COUNT);
   const comments = [];
 
@@ -46,8 +58,6 @@ const getComments = () => {
 };
 
 const getRandomDate = () => {
-  const MAX_DATE_GAP = 25550;
-
   const dateGap = getRandomInteger(MAX_DATE_GAP);
   const currentDate = new Date();
 
@@ -57,11 +67,6 @@ const getRandomDate = () => {
 };
 
 const getRandomWriters = () => {
-  const WritersCount = {
-    MIN: 1,
-    MAX: 2,
-  };
-
   const writersCount = getRandomInteger(WritersCount.MIN, WritersCount.MAX);
 
   return Array(writersCount).fill(``).map(() => generateRandomItem(WRITERS));
@@ -69,20 +74,12 @@ const getRandomWriters = () => {
 };
 
 const getRandomActors = () => {
-  const ActorsCount = {
-    MIN: 3,
-    MAX: 5,
-  };
   const actorsCount = getRandomInteger(ActorsCount.MIN, ActorsCount.MAX);
 
   return Array(actorsCount).fill(``).map(() => generateRandomItem(ACTORS));
 };
 
 const getRandomGenres = () => {
-  const GenresCount = {
-    MIN: 1,
-    MAX: 3,
-  };
   const genresCount = getRandomInteger(GenresCount.MIN, GenresCount.MAX);
 
   return Array(genresCount).fill(``).map(() => generateRandomItem(ACTIONS));
